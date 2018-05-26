9e72bda58d5282cdad5e13252b471923a4a0334e
Arakawa Shintaro <arashinbe@gmail.com>
2018-05-27 01:50:02 +0900

    exported names

--- hello.go
+++ hello.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 	"time"
 )
 
 func main() {
 	fmt.Println("Welcom to the playgound!")
 	fmt.Println("The time is", time.Now())
+	fmt.Println(rand.Intn(10))
+	fmt.Println(math.Pi)
 }
